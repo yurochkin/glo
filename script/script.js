b9ce7b799ec54fe19eab9f7ed9a07112447d611e
Vladimir_Work <yurochkin.v@gmail.com>
2021-08-02 10:05:31 +0200

    Second lesson

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert('сообщение');
-console.log('сообщение с любым текстом');
+let money = 666;
+let income = 'freelance';
+let addExpenses = 'internet, taxi, utilities payments' ;
+let deposit = true;
+let mission = 777;
+let period = 8;
+
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
+console.log('length_addExpenses: ', addExpenses.length);
+console.log('Период равен ${period} месяцев.');
+console.log('Цель заработать ${mission} рублей/долларов/гривен/юани');
+console.log(addExpenses.toLocaleLowerCase().split(', '));
+const budgetDay = (money / 30);
+console.log('budget:', budgetDay);

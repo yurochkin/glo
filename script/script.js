8a9546750013941bf43a26fa38fec9ed056fe21e
Vladimir_Work <yurochkin.v@gmail.com>
2021-08-06 11:07:26 +0200

    lesson04

--- script/script.js
+++ script/script.js
@@ -1,9 +1,16 @@
-let money = 666;
+'use strict';
+let money = prompt('Ваш месячный доход?');
 let income = 'freelance';
-let addExpenses = 'internet, taxi, utilities payments' ;
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов');
+let amount2 = +prompt('Во сколько это обойдется?');
 let mission = 777;
 let period = 8;
+
+
 
 console.log('money: ', typeof money);
 console.log('income: ', typeof income);
@@ -12,5 +19,35 @@ console.log('length_addExpenses: ', addExpenses.length);
 console.log('Период равен ${period} месяцев.');
 console.log('Цель заработать ${mission} рублей/долларов/гривен/юани');
 console.log(addExpenses.toLocaleLowerCase().split(', '));
-const budgetDay = (money / 30);
-console.log('budget:', budgetDay);
+
+const getExpensesMonth = () => {
+    return amount1 + amount2;
+};
+const getAccumulatedMonth = (moneyMonth, expensesMonth) => {
+    return moneyMonth - expensesMonth;
+};
+const accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth());
+
+const getTargetMonth = (pyrp, budgetMonth) => {
+    return Math.ceil(pyrp / budgetMonth);
+};
+
+
+const budgetDay = accumulatedMonth / 30;
+
+
+console.log('Бюджет на день: ', Math.floor(budgetDay));
+if (budgetDay === 1200) {
+    console.log('Почти успеха достиг');
+} else if (budgetDay === 600) {
+    console.log('почти средний уровень дохода');
+} else if (budgetDay > 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay < 1200 && budgetDay > 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay < 0) {
+    console.log('Что то пошло не так');
+} else {
+    console.log('уровень дохода ниже среднего');
+}
+
